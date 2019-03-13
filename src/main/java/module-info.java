f50dcf079b81afb017a4d0b2935a31915f4cba6d
Bernd Eilers <bernd.eilers@fms-logistics.com>
2019-03-13 16:00:52 +0100

    delete module-info

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module net.agilhard.jsch {
-    requires transitive java.security.jgss;
-    requires net.agilhard.jzlib;
-    exports net.agilhard.jsch;
-    exports net.agilhard.jsch.jce;
-    exports net.agilhard.jsch.jcraft;
-    exports net.agilhard.jsch.jgss;
-
-}
